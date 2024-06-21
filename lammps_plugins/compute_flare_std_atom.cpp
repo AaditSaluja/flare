fbc4eec40afb8f07785219f6e2108f7b8e94d121
AaditSaluja <aaditsaluja@gmail.com>
2024-06-21 15:15:50 -0400

    Add fgets

--- lammps_plugins/compute_flare_std_atom.cpp
+++ lammps_plugins/compute_flare_std_atom.cpp
@@ -387,6 +387,7 @@ void ComputeFlareStdAtom::read_file(char *filename) {
     sscanf(line, "%s", kernel_string); // kernel name
     kernel_string_length = strlen(kernel_string);
 
+    fgets(line, MAXLINE, fptr);
     sscanf(line, "%s", bodyorder_string); // Body order B1/2
     if (strcmp(body_order_string, "B2")) {
       error->all(FLERR, "Descriptors need to be B2");
@@ -522,9 +523,8 @@ void ComputeFlareStdAtom::read_L_inverse(char *filename) {
     hyperparameters(3) = sn;
 
     fgets(line, MAXLINE, fptr);
-
+    
     sscanf(line, "%s", bodyorder_string); // Body order B1/2
-
     if (strcmp(bodyorder_string, "B2")) {
       error->all(FLERR, "Descriptors need to be B2");
     }

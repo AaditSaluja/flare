4943944f8dc05413247f4f07118b82f779e69a0f
AaditSaluja <aaditsaluja@gmail.com>
2024-07-15 16:26:14 -0400

    test 3

--- lammps_plugins/pair_flare_b2.h
+++ lammps_plugins/pair_flare_b2.h
@@ -1,56 +1,56 @@
-// // Jonathan Vandermause
-// // Pair style based on pair_eam.h
+// Jonathan Vandermause
+// Pair style based on pair_eam.h
 
-// #ifdef PAIR_CLASS
+#ifdef PAIR_CLASS
 
-// PairStyle(flare/b2, PairFLAREB2)
+PairStyle(flare/b2sep, PairFLAREB2sep)
 
-// #else
+#else
 
-// #ifndef LMP_PAIR_FLARE_B2_H
-// #define LMP_PAIR_FLARE_B2_H
+#ifndef LMP_PAIR_FLARE_B2_H
+#define LMP_PAIR_FLARE_B2_H
 
-// #include "pair.h"
-// #include <Eigen/Dense>
-// #include <cstdio>
-// #include <vector>
+#include "pair.h"
+#include <Eigen/Dense>
+#include <cstdio>
+#include <vector>
 
-// namespace LAMMPS_NS {
+namespace LAMMPS_NS {
 
-// class PairFLAREB2 : public Pair {
-// public:
-//   PairFLAREB2(class LAMMPS *);
-//   virtual ~PairFLAREB2();
-//   virtual void compute(int, int);
-//   void settings(int, char **);
-//   virtual void coeff(int, char **);
-//   virtual void init_style();
-//   double init_one(int, int);
+class PairFLAREB2sep : public Pair {
+public:
+  PairFLAREB2sep(class LAMMPS *);
+  virtual ~PairFLAREB2sep();
+  virtual void compute(int, int);
+  void settings(int, char **);
+  virtual void coeff(int, char **);
+  virtual void init_style();
+  double init_one(int, int);
 
-// protected:
-//   int power, n_species, n_max, l_max, n_descriptors, beta_size;
-//   bool normalized;
+protected:
+  int power, n_species, n_max, l_max, n_descriptors, beta_size;
+  bool normalized;
 
-//   std::function<void(std::vector<double> &, std::vector<double> &, double, int,
-//                      std::vector<double>)>
-//       basis_function;
-//   std::function<void(std::vector<double> &, double, double,
-//                      std::vector<double>)>
-//       cutoff_function;
+  std::function<void(std::vector<double> &, std::vector<double> &, double, int,
+                     std::vector<double>)>
+      basis_function;
+  std::function<void(std::vector<double> &, double, double,
+                     std::vector<double>)>
+      cutoff_function;
 
-//   std::vector<double> radial_hyps, cutoff_hyps;
+  std::vector<double> radial_hyps, cutoff_hyps;
 
-//   double cutoff;
-//   double *beta, *cutoffs;
-//   Eigen::MatrixXd beta_matrix, cutoff_matrix;
-//   std::vector<Eigen::MatrixXd> beta_matrices;
+  double cutoff;
+  double *beta, *cutoffs;
+  Eigen::MatrixXd beta_matrix, cutoff_matrix;
+  std::vector<Eigen::MatrixXd> beta_matrices;
 
-//   virtual void allocate();
-//   virtual void read_file(char *);
-//   void grab(FILE *, int, double *);
-// };
+  virtual void allocate();
+  virtual void read_file(char *);
+  void grab(FILE *, int, double *);
+};
 
-// } // namespace LAMMPS_NS
+} // namespace LAMMPS_NS
 
-// #endif
-// #endif
+#endif
+#endif
